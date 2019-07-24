cf60de1b190314a914948ed2d57370eb6327367c
leepoo <leepoo2008@163.com>
2019-07-25 01:23:07 +0800

    添加头部导航样式

--- components/public/header/index.vue
+++ components/public/header/index.vue
@@ -1,26 +1,28 @@
 <template>
-    <el-row :gutter="15" class="m-header">
-      <el-col :span="6">
+    <el-row  class="m-header">
+      <el-col :span="4">
         <geo/>
       </el-col>
       <el-col :span="6">
         <user/>
       </el-col>
-      <el-col :span="6">
-        <div class="grid-content bg-purple"></div>
+      <el-col :span="14">
+       <nav-bar/>
       </el-col>
     </el-row>
 </template>
 <script>
 import Geo from './geo'
 import User from './user'
+import NavBar from './nav'
 export default {
   components: {
     Geo,
-    User
+    User,
+    NavBar
   }
 }
 </script>
-<style lang="scss" scoped>
-  @import url('@/assets/css/public/layout.scss');
+<style lang="scss">
+  @import "@/assets/css/public/header/index.scss";
 </style>

72eb35fd269ebaec837ff39110b1fcaa92d87ddf
leepoo <leepoo2008@163.com>
2019-07-26 01:17:27 +0800

    完成首页大致布局

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,24 @@
 <template>
     <el-container class="layout-default">
-      <el-header>
+      <el-header height="197px">
         <mt-Header />
       </el-header>
       <el-main>
         <nuxt />
       </el-main>
-      <el-footer>Footer</el-footer>
+      <el-footer height="100%">
+        <mt-Footer />
+      </el-footer>
     </el-container>
 </template>
 <script>
 import mtHeader from '@/components/public/header/index.vue'
+import mtFooter from '@/components/public/footer/index.vue'
+
 export default {
   components: {
-    mtHeader
+    mtHeader,
+    mtFooter
   }
 }
 </script>
